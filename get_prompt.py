5e0d45fb9e1cf983bf53b9ef0e95f2a28fcee04c
ishnoor singh <m22aie233@iitj.ac.in>
2023-12-04 02:29:05 +0530

    adding changes to streamlit app

--- get_prompt.py
+++ get_prompt.py
@@ -6,40 +6,17 @@ def generate_prompt_template():
     Returns a Langchain prompt template
     """
     CYPHER_GENERATION_TEMPLATE =  """
-        You are an assistant whose task is to Generate Cypher statement to query a graph database and return results in human readable format which will in turn be used by medical profeesionals to read clinical trials
- 
-        Instructions:
-        - Use only the information stored in the graph.Do not use any external information in your answer.
-        - Use only the provided relationship types and properties in the schema.
-        - The answer should only Include only the provided information from the query.Nothing else should be included.
- 
-        Schema (Nodes, Relationships, Properties):
-        {schema}
- 
-        Examples:
-            # Find all trials studying a specific disease like Malaria:
-            MATCH (t:Trial)-[:TARGETS]->(d:Disease {Name: "Malaria"})
-            RETURN t.TrialID, t.Title, t.Phase, t.Status
+            You are an assistant whose task is to Generate Cypher statement to query a graph database and return results in human readable format which will in turn be used by medical profeesionals to read clinical trials
 
-            # Find all interventions used in a specific trial:
-            MATCH (t:Trial {TrialID: "NCT123456"})-[:USES]->(i:Intervention)
-            RETURN i.Name, i.Type
+            Instructions:
+            - Use only the provided relationship types and properties in the schema.
+            - Do not use any other relationship types or properties that are not provided.
+            - The values stored in graph database are in lowercase , so convert the query in lowercase before processing.
+            Schema (Nodes, Relationships, Properties):
+            {schema}
 
-            # List all the trials conducted in a specific country:
-            MATCH (t:Trial)-[:CONDUCTED_AT]->(l:Location {Country: "India"})
-            RETURN t.TrialID, t.Title, t.Status
-
-            # Find all trials and their locations:
-            MATCH (t:Trial)-[:CONDUCTED_AT]->(l:Location)
-            RETURN t.TrialID, t.Title, l.City, l.Country
-
-            # Find all trials that use a specific type of intervention, like a Drug:
-            MATCH (t:Trial)-[:USES]->(i:Intervention {Type: "Drug"})
-            RETURN t.TrialID, t.Title, i.Name
-
- 
-        The question is:
-        {question}
+            The question is:
+            {question}
         """
     CYPHER_GENERATION_PROMPT = PromptTemplate(
         input_variables=["schema", "question"], template=CYPHER_GENERATION_TEMPLATE
